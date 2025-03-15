e9ccccfa693c5e214c387ba3f38dee39032427ee
NitroSniper <mahiemiah35@gmail.com>
2025-03-15 21:00:00 +0000

    Example application setup

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A flake for Node and Postgresql";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        # https://wiki.nixos.org/wiki/Python#Possible_Optimizations
+        pkgs = import inputs.nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_23
+          ];
+        };
+      }
+    );
+}
